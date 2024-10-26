0d2f4c34ff8f6f4129ba3ee659f588b6abafc205
Sourabh <98006817+Sourabh908@users.noreply.github.com>
2024-10-26 15:37:04 +0530

    Update Find_the_maximal_number_of_non-intersecting_segments_having_sum_zero.cpp

--- Find_the_maximal_number_of_non-intersecting_segments_having_sum_zero.cpp
+++ Find_the_maximal_number_of_non-intersecting_segments_having_sum_zero.cpp
@@ -150,56 +150,9 @@ bool binary_search(vector<int> &nums,int x){
 // indexed_set s;
 // (n+p-1)/p 
 /*
-  D. Kousuke's Assignment
-time limit per test2 seconds
-memory limit per test256 megabytes
-After a trip with Sakurako, Kousuke was very scared because he forgot about his programming assignment. In this assignment, the teacher gave him an array a
- of n
- integers and asked him to calculate the number of non-overlapping segments of the array a
-, such that each segment is considered beautiful.
-
-A segment [l,r]
- is considered beautiful if al+al+1+⋯+ar−1+ar=0
-.
-
-For a fixed array a
-, your task is to compute the maximum number of non-overlapping beautiful segments.
-
-Input
-The first line of input contains the number t
- (1≤t≤104
-) — the number of test cases. Each test case consists of 2
- lines.
-
-The first line contains one integer n
- (1≤n≤105
-) — the length of the array.
-The second line contains n
- integers ai
- (−105≤ai≤105
-) — the elements of the array a
-.
-It is guaranteed that the sum of n
- across all test cases does not exceed 3⋅105
-.
-
-Output
-For each test case, output a single integer: the maximum number of non-overlapping beautiful segments.
-
-Example
-InputCopy
-3
-5
-2 1 -3 2 1
-7
-12 -4 4 43 -3 -5 8
-6
-0 -4 0 3 0 1
-OutputCopy
-1
-2
-3
-  */
+  Question Link: 
+  https://codeforces.com/contest/2033/problem/D 
+*/
 
 void answer() {
     
